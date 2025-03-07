5a2185705653c848ba96186520b8e4b3520acda7
SEVDA RIZAYEVA <28RIZAYEVAS@live.bwschools.net>
2025-03-07 13:24:02 -0500

    Initial commit

---
+++ olympics.py
@@ -0,0 +1,51 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('tan')
+t = turtle.Turtle()
+
+t.pencolor('blue')
+t.penup()
+t.goto(-80,15)
+t.pendown()
+t.circle(35)
+t.penup()
+
+t.pencolor('red')
+t.goto(80,15)
+t.pendown()
+t.circle(35)
+t.penup()
+
+t.pencolor('black')
+t.goto(0,15)
+t.pendown()
+t.circle(35)
+t.penup()
+
+t.goto(-40,-15)
+t.pencolor('yellow')
+t.pendown()
+t.circle(35)
+t.penup()
+
+t.goto(40,-15)
+t.pencolor('green')
+t.pendown()
+t.circle(35)
+
+t.penup()
+t.goto(-125,100)
+t.pencolor("purple")
+t.pendown()
+
+t.write("Winter Olympics",font=("Arial",30,"bold"))
+
+t.penup()
+t.goto(-40,-100)
+t.pencolor('purple')
+t.pendown()
+
+
+t.write("2026",font=("Arial",30,"bold"))
